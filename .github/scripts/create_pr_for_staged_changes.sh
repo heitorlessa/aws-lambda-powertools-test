8e43e680eb9f51070d124180b6b925b9c9a2b157
heitorlessa <lessa@amazon.co.uk>
2023-05-07 00:13:23 +0200

    chore(ci): final lint pass

--- .github/scripts/create_pr_for_staged_changes.sh
+++ .github/scripts/create_pr_for_staged_changes.sh
@@ -43,8 +43,8 @@ function push_staged_files_to_temp_branch() {
     debug "Creating branch ${TEMP_BRANCH}"
     git checkout -b "${TEMP_BRANCH}"
 
-    debug "Committing staged files: $@"
-    git add $@
+    debug "Committing staged files: $*"
+    git add "$@"
     git commit -m "${COMMIT_MSG}"
 
     debug "Creating branch remotely"
@@ -53,16 +53,19 @@ function push_staged_files_to_temp_branch() {
 
 function create_pr() {
     debug "Creating PR against ${BRANCH} branch"
-    export NEW_PR_URL=$(gh pr create --title "${PR_TITLE}" --body "${PR_BODY}: ${WORKFLOW_URL}" --base "${BRANCH}") # https://github.com/awslabs/aws-lambda-powertools/pull/13
-    export NEW_PR_ID="${NEW_PR_URL##*/}"                                                                            # 13
+    NEW_PR_URL=$(gh pr create --title "${PR_TITLE}" --body "${PR_BODY}: ${WORKFLOW_URL}" --base "${BRANCH}") # https://github.com/awslabs/aws-lambda-powertools/pull/13
+    NEW_PR_ID="${NEW_PR_URL##*/}"                                                                            # 13
+    export NEW_PR_URL
+    export NEW_PR_ID
 }
 
 function close_duplicate_prs() {
     debug "Do we have any duplicate PRs?"
-    export DUPLICATE_PRS=$(gh pr list --search "${PR_TITLE}" --json number --jq ".[] | select(.number != ${NEW_PR_ID}) | .number")
+    DUPLICATE_PRS=$(gh pr list --search "${PR_TITLE}" --json number --jq ".[] | select(.number != ${NEW_PR_ID}) | .number")
 
     debug "Closing duplicated PRs if any"
     echo "${DUPLICATE_PRS}" | xargs -L1 gh pr close --delete-branch --comment "Superseded by #${NEW_PR_ID}"
+    export DUPLICATE_PRS
 }
 
 function report_summary() {
@@ -80,11 +83,11 @@ function main() {
     check_staged_files
     check_env_vars
 
-    push_staged_files_to_temp_branch $@
+    push_staged_files_to_temp_branch "$@"
     create_pr
     close_duplicate_prs
 
     report_summary
 }
 
-main $@
+main "$@"
