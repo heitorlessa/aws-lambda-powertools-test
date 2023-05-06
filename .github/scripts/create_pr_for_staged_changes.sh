5f4699d43195aaa0731d2f40bb3757df73ebc67e
heitorlessa <lessa@amazon.co.uk>
2023-05-06 23:38:01 +0200

    chore(ci): make script and workflow reusable

--- .github/scripts/create_pr_for_staged_changes.sh
+++ .github/scripts/create_pr_for_staged_changes.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-readonly COMMIT_MSG="chore(ci): update changelog with latest changes"
-readonly PR_TITLE="chore(ci): changelog rebuild"
+#title              :create_pr_for_staged_changes.sh
+#description        :This script will create a PR for staged changes, detect and close duplicate PRs with the same title, and craft a GitHub Actions report at the end.
+#author		        :@heitorlessa
+#date               :May 8th 2023
+#version            :0.1
+#usage		        :bash create_pr_for_staged_changes.sh
+#notes              :Install Vim and Emacs to use this script.
+#os_version         :Ubuntu   22.04.2 LTS
+#required_env_vars  :COMMIT_MSG, PR_TITLE, TEMP_BRANCH, GH_TOKEN, RUN_ID, WORKFLOW_URL
+#==============================================================================
+
 readonly PR_BODY="This is an automated PR created from the following workflow"
 
 # Sets GitHub Action with error message to ease troubleshooting
@@ -37,17 +46,18 @@ git push origin "${TEMP_BRANCH}"
 
 debug "Creating PR against ${BRANCH} branch"
 NEW_PR_URL=$(gh pr create --title "${PR_TITLE}" --body "${PR_BODY}: ${WORKFLOW_URL}" --base "${BRANCH}") # https://github.com/awslabs/aws-lambda-powertools/pull/13
-
-echo "PR URL: ${NEW_PR_URL}"
-
-NEW_PR_ID=$(echo "${NEW_PR_URL##*/}") # 13
-echo "PR ID: ${NEW_PR_ID##*/}"
+NEW_PR_ID=$(echo "${NEW_PR_URL##*/}")                                                                    # 13
 
 debug "Do we have any duplicate PRs?"
 DUPLICATE_PRS=$(gh pr list --search "${PR_TITLE}" --json number --jq ".[] | select(.number != ${NEW_PR_ID}) | .number")
 
 debug "Closing duplicated PRs if any"
-echo "${DUPLICATE_PRS}" | xargs -L1 gh pr close --delete-branch --comment "Superseded by ${NEW_PR_ID}"
+echo "${DUPLICATE_PRS}" | xargs -L1 gh pr close --delete-branch --comment "Superseded by #${NEW_PR_ID}"
 
 debug "Creating job summary"
-echo "### Pull request created successfully! :rocket: ${NEW_PR_ID}. Closed duplicated PRs ${DUPLICATE_PRS}" >>$GITHUB_STEP_SUMMARY
+echo "### Pull request created successfully! :rocket: #${NEW_PR_ID}. \n\n Closed duplicated PRs (if any): ${DUPLICATE_PRS}" >>$GITHUB_STEP_SUMMARY
+
+debug "Creating job outputs"
+echo "PR_URL=${NEW_PR_URL}" >>"$GITHUB_OUTPUT"
+echo "PR_BRANCH=${TEMP_BRANCH}" >>"$GITHUB_OUTPUT"
+echo "PR_DUPLICATES=${DUPLICATE_PRS}" >>"$GITHUB_OUTPUT"
