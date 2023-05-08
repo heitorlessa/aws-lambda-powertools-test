429084002b3d4205913c4b44740a38e613f8d7bf
heitorlessa <lessa@amazon.co.uk>
2023-05-08 12:12:38 +0200

    chore(ci): remove simulated failure

--- .github/scripts/create_pr_for_staged_changes.sh
+++ .github/scripts/create_pr_for_staged_changes.sh
@@ -105,7 +105,6 @@ function main() {
     has_required_config
 
     create_temporary_branch_with_changes "$@"
-    simulate_failure_to_test_branch_cleanup || exit 1
     create_pr
     close_duplicate_prs
 
