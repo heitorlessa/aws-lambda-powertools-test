64163d17af6b2bebdd3cc9cb13976ce4b8fea049
heitorlessa <lessa@amazon.co.uk>
2023-05-10 09:44:49 +0200

    chore: introduce spans to ease debugging when debug logs are off

--- .github/scripts/create_pr_for_staged_changes.sh
+++ .github/scripts/create_pr_for_staged_changes.sh
@@ -27,8 +27,16 @@ function notice() {
     echo "::notice file=${FILENAME}::$1"
 }
 
+function start_span() {
+    echo "::group::$1"
+}
+
+function end_span() {
+    echo "::endgroup::"
+}
+
 function has_required_config() {
-    debug "Do we have required environment variables?"
+    start_span "Validating required config"
     test -z "${TEMP_BRANCH_PREFIX}" && error "TEMP_BRANCH_PREFIX env must be set to create a PR"
     test -z "${PR_TITLE}" && error "PR_TITLE env must be set"
     test -z "${GH_TOKEN}" && error "GH_TOKEN env must be set for GitHub CLI"
@@ -40,48 +48,54 @@ function has_required_config() {
     test -z "${GITHUB_REPOSITORY}" && error "GITHUB_REPOSITORY env must be set to trace Workflow Run ID back to PR"
 
     set_environment_variables
+    end_span
 }
 
 function set_environment_variables() {
+    start_span "Setting environment variables"
     export readonly WORKFLOW_URL="${GITHUB_SERVER_URL}"/"${GITHUB_REPOSITORY}"/actions/runs/"${GITHUB_RUN_ID}" # e.g., heitorlessa/aws-lambda-powertools-test/actions/runs/4913570678
     export readonly TEMP_BRANCH="${TEMP_BRANCH_PREFIX}"-"${GITHUB_RUN_ID}"                                     # e.g., ci-changelog-4894658712
     export readonly BASE_BRANCH="${BASE_BRANCH:-develop}"                                                      # e.g., main, defaults to develop if missing
     export readonly PR_BODY="This is an automated PR created from the following workflow"
     export readonly FILENAME=".github/scripts/$(basename "$0")"
     export readonly NO_DUPLICATES_MESSAGE="No duplicated PRs found"
+    end_span
 }
 
 function has_anything_changed() {
-    debug "Is there an update to the source code?"
+    start_span "Validating git staged files"
     HAS_ANY_SOURCE_CODE_CHANGED="$(git status --porcelain)"
 
     test -z "${HAS_ANY_SOURCE_CODE_CHANGED}" && echo "Nothing to update" && exit 0
+    end_span
 }
 
 function create_temporary_branch_with_changes() {
-    debug "Creating branch ${TEMP_BRANCH}"
+    start_span "Creating temporary branch: "${TEMP_BRANCH}""
     git checkout -b "${TEMP_BRANCH}"
 
     debug "Committing staged files: $*"
     git add "$@"
     git commit -m "${PR_TITLE}"
 
-    debug "Creating branch remotely"
     git push origin "${TEMP_BRANCH}"
+    end_span
 }
 
 function create_pr() {
-    debug "Creating PR against ${TEMP_BRANCH} branch"
+    start_pan "Creating PR against ${TEMP_BRANCH} branch"
     NEW_PR_URL=$(gh pr create --title "${PR_TITLE}" --body "${PR_BODY}: ${WORKFLOW_URL}" --base "${BASE_BRANCH}" || error "Failed to create PR") # e.g, https://github.com/awslabs/aws-lambda-powertools/pull/13
 
     # greedy remove any string until the last URL path, including the last '/'. https://opensource.com/article/17/6/bash-parameter-expansion
+    debug "Extracing PR Number from PR URL "${NEW_PR_URL}""
     NEW_PR_ID="${NEW_PR_URL##*/}" # 13
     export NEW_PR_URL
     export NEW_PR_ID
+    end_span
 }
 
 function close_duplicate_prs() {
-    debug "Do we have any duplicate PRs?"
+    start_span "Searching for duplicate PRs"
     DUPLICATE_PRS=$(gh pr list --search "${PR_TITLE}" --json number --jq ".[] | select(.number != ${NEW_PR_ID}) | .number") # e.g, 13\n14
 
     if [ -z "${DUPLICATE_PRS}"]; then
@@ -92,15 +106,17 @@ function close_duplicate_prs() {
     fi
 
     export readonly DUPLICATE_PRS
+    end_span
 }
 
 function report_summary() {
-    debug "Creating job summary"
+    start_pan "Creating job summary"
     echo "### Pull request created successfully :rocket: ${NEW_PR_URL} <br/><br/> Closed duplicated PRs: ${DUPLICATE_PRS}" >>"$GITHUB_STEP_SUMMARY"
 
     notice "PR_URL is ${NEW_PR_URL}"
     notice "PR_BRANCH is ${TEMP_BRANCH}"
     notice "PR_DUPLICATES are ${DUPLICATE_PRS}"
+    end_span
 }
 
 function main() {
@@ -110,7 +126,6 @@ function main() {
 
     create_temporary_branch_with_changes "$@"
     create_pr
-    error "Force cleanup"
     close_duplicate_prs
 
     report_summary
