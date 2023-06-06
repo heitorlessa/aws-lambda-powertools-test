1f0919f2232e68164718cd35f677242d56517398
heitorlessa <lessa@amazon.co.uk>
2023-06-06 10:18:52 +0200

    chore: sync version files

--- aws_lambda_powertools/__init__.py
+++ aws_lambda_powertools/__init__.py
@@ -4,11 +4,14 @@
 
 from pathlib import Path
 
+from aws_lambda_powertools.shared.version import VERSION
+
 from .logging import Logger
 from .metrics import Metrics, single_metric
 from .package_logger import set_package_logger_handler
 from .tracing import Tracer
 
+__version__ = VERSION
 __author__ = """Amazon Web Services"""
 __all__ = [
     "Logger",
